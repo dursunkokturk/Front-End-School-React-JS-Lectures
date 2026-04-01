d36b18ca769e6f2ecebe9a0876dfc7217cbdf2ea
Dursun Kokturk <dursunserefkokturk@gmail.com>
2026-04-01 21:36:10 +0300

    part-1-es-6-before klasoru icindeki lecture-1-variable-declaration-before-es-6 klasoru icinde degisken tanimlama isleminin eski kullanimi gosterildi,part-2-es-6-after klasoru icindeki lecture-1-variable-declaration-after-es-6 klasoru icinde degisken tanimlama isleminin es-6 itibari ile kullanimi gosterildi

---
+++ Part-1-ES-6-Before/Lecture-1-Variable-Declaration-Before-ES-6/assets/js/before-es-6-.js
@@ -0,0 +1,3 @@
+// ES-6 Oncesi Degisken Tanimlama Islemi
+var name = "Dursun";
+console.log("Merhaba" + ' ' + name);
---
+++ Part-2-ES-6-After/Lecture-1-Variable-Declaration/assets/js/after-es-6-.js
@@ -0,0 +1,3 @@
+// ES-6 Oncesi Degisken Tanimlama Islemi
+const name = "Dursun";
+console.log("Merhaba" + ' ' + name);
