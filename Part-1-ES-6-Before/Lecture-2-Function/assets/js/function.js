3dd8ce4679a50b09d97463ea043dcb9b6925f626
Dursun Kokturk <dursunserefkokturk@gmail.com>
2026-04-01 21:40:46 +0300

    part-1-es-6-before klasoru icindeki lecture-2-function klasoru icinde fonksiyon tanimlama isleminin eski kullanimi gosterildi,part-2-es-6-after klasoru icindeki lecture-2-arrow-function klasoru icinde fonksiyon tanimlama isleminin es-6 itibari ile kullanimi gosterildi

---
+++ Part-1-ES-6-Before/Lecture-2-Function/assets/js/function.js
@@ -0,0 +1,10 @@
+function total(a, b) {
+  return a + b;
+}
+
+const numbers = [1, 2, 3];
+const twiceAsMuch = numbers.map(function (x) {
+  return x * 2;
+})
+
+console.log(twiceAsMuch);
---
+++ Part-2-ES-6-After/Lecture-2-Arrow-Function/assets/js/arrow-function.js
@@ -0,0 +1,6 @@
+const total = (a, b) => a + b;
+
+const numbers = [1, 2, 3];
+const twiceAsMuch = numbers.map(x => x * 2);
+
+console.log(twiceAsMuch);
