99336399ac0b4b5564c5173bed8049bf097dc01a
Dursun Kokturk <dursunserefkokturk@gmail.com>
2026-04-01 21:45:53 +0300

    part-1-es-6-before klasoru icindeki lecture-3-destructuring klasoru icinde obje icindeki degerleri yazdirma isleminin eski kullanimi gosterildi,part-2-es-6-after klasoru icindeki lecture-3-template-literals klasoru icinde obje icindeki degerleri yazdirma isleminin es-6 itibari ile kullanimi gosterildi

---
+++ Part-1-ES-6-Before/Lecture-3-Destructuring/assets/js/destructuring.js
@@ -0,0 +1,14 @@
+const student = {
+  name: "Dursun",
+  age: 20
+}
+
+const studentName = student.name;
+const studentAge = student.age;
+
+console.log(studentName);
+console.log(studentAge);
+
+const colors = [red,green,yellow];
+const firstColor = colors[0];
+const endColor = colors[2];
---
+++ Part-2-ES-6-After/Lecture-3-Template-Literals/assets/js/template-literals.js
@@ -0,0 +1,6 @@
+const firstName = "Dursun";
+const lastName = "Köktürk";
+const age = 38;
+const message = `Merhaba ben ${firstName} ${lastName} ${age} yaşındayım`
+
+console.log(message);
