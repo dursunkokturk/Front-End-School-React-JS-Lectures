4483ab6938f2f2295628ee81d84e3cd42fccee32
Dursun Kokturk <dursunserefkokturk@gmail.com>
2026-04-03 19:29:37 +0300

    first-react-js-lecture klasoru icinde karsilama ekrani olusturuldu, component olusturma ve ekrana icerik yazdirma islemleri yapildi, kullanici giris yapma durumuna gore gosterilecek componentler duzenlend,, counter component'i icinde islemler devam edecek

---
+++ First-React-JS-Lecture/src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+export default function App() {
+  const isLogin = true;
+  return (
+    <>
+      <h1>Hello React World</h1>
+      <MyButton />
+      <Profile />
+      {/* <Admin /> */}
+      {/* <Login /> */}
+      {isLogin ? <Admin /> : <Login />}
+      <Counter />
+    </>
+  )
+}
+
+function Counter() {
+
+  // count Yapilacak Islemin Atandigi Degisken
+  // setCount count Degiskeni Uzerinden Yapilan Islemin Guncel Halini Barindiran Kisim
+  // useState count Degiskeninin Baslangic Degerini Barindiran Hook
+  const [count, setCount] = useState(0);
+
+  function handleIncrease() {
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <button onClick={handleIncrease}>Tıkla</button>
+    </>
+  )
+}
+
+function MyButton() {
+  return (
+    <>
+      <button>Tıkla</button>
+    </>
+  )
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+function Admin() {
+  return (
+    <>
+      <h2>Admin Sayfası</h2>
+      <p>Hoş Geldin Admin</p>
+    </>
+  )
+}
+
+function Login() {
+  return (
+    <>
+      <h2>Kayıt Sayfası</h2>
+      <form action="">
+        <input type="text" placeholder="Kullanıcı Adı" />
+        <br />
+        <input type="password" placeholder="Kullanıcı Şifresi" />
+        <br />
+        <button>Giriş Yap</button>
+      </form>
+    </>
+  )
+}
