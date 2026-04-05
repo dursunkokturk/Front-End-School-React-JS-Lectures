e641ec677c94012c9e1aba363a2194b751394b24
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-05 22:20:39 +0300

    react-js-lectures klasoru icindeki src klasoru icindeki app.jsx dosyasinda buton fonksiyonu olusturuldu, app.jsx dosyasindaki app function icinde buton fonksiyonu component olarak eklendi

--- React-JS-Lectures/src/App.jsx
+++ React-JS-Lectures/src/App.jsx
@@ -2,6 +2,15 @@ export default function App() {
   return (
     <>
       <h1>Hello React World</h1>
+      <MyButton />
+    </>
+  )
+}
+
+function MyButton() {
+  return (
+    <>
+      <button>Tıkla</button>
     </>
   )
 }
