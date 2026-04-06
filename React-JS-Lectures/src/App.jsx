06d0e1f74a42eb0e5a3be90d4b90c0ef311fa31a
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-06 21:24:05 +0300

    react-js-lectures klasoru icindeki src klasoru icindeki app.jsx dosyasinda admin ve login fonksiyonlari olusturuldu, app.jsx dosyasindaki app function icinde admin girisi yapildiginda admin component calisiyor, admin girisi yapilmadiginda login component calisiyor

--- React-JS-Lectures/src/App.jsx
+++ React-JS-Lectures/src/App.jsx
@@ -1,9 +1,35 @@
 export default function App() {
+  const isLogin = false;
   return (
     <>
       <h1>Hello React World</h1>
       <MyButton />
       <Profile />
+      {isLogin ? <Admin /> : <Login />}
+    </>
+  )
+}
+
+function Admin() {
+  return (
+    <>
+      <h2>Admin Sayfası</h2>
+      <p>Hoş Geldin Admin</p>
+    </>
+  )
+}
+
+function Login() {
+  return (
+    <>
+      <h2>Kayıt Sayfası</h2>
+      <form action="">
+        <input type="text" placeholder="Kullanıcı Adı" />
+        <br />
+        <input type="password" placeholder="Kullanıcı Şifresi" />
+        <br />
+        <button>Giriş Yap</button>
+      </form>
     </>
   )
 }
