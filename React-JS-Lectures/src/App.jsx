146a4f5f41e7ab1e93adf206be05f0a75ff8d6e5
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-08 07:03:07 +0300

    react-js-lectures klasoru icindeki src klasoru icindeki app.jsx dosyasinda counter1 fonksiyonunda sayi artirma ve azaltma islemi arrow function kullanilarak yapildi

--- React-JS-Lectures/src/App.jsx
+++ React-JS-Lectures/src/App.jsx
@@ -33,6 +33,7 @@ export default function App() {
       <Counter />
       <Hi />
       <MyApp />
+      <Counter1 />
     </>
   )
 }
@@ -113,6 +114,25 @@ function Counter() {
       <button onClick={handleDecrease} disabled={count === 0}>-</button>
       <button>Sayaç {count}</button>
       <button onClick={handleIncrease}>+</button>
+    </>
+  )
+}
+
+// Sayi Artirma Ve Azaltma Islemi Icin 
+// Arrow Function Kullaniyoruz
+function Counter1() {
+
+  // count : Yapilacak Islemin Atandigi Degisken
+  // setCount : count Degiskeni Uzerinden Yapilan Islemin Guncel Halini Barindiran Kisim
+  // useState : count Degiskeninin Baslangic Degerini Barindiran Hook
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/* <button onClick={handleIncrease}>Tıkla {count}</button> */}
+      <button onClick={() => setCount(count - 1)} disabled={count === 0}>-</button>
+      <button>Sayaç {count}</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </>
   )
 }
